f3a30b3cc66df612db7123e6d4d323010c4276f2
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-15 14:39:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,11 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-   bash_hash = {:railroads => {}}
+   bash_hash = {:railroads => {
+     :pieces => 4,
+     :rent_in_dollars => {},
+     :names => {}
+   }}
    
    
   # When you start writing the implementation for this method, copy the Hash
