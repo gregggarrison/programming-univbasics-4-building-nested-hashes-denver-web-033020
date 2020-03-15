86f6d6ea2b48c71d9a9430cd88bc99e1e7ef3a5a
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-15 14:28:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def base_hash
-  bash_hash = {:railroads {}}
+  bash_hash = {:railroads => {}}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 
